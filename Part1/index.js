616412cd5e58fe9f6cabc9f52a49f965db32165b
Cathy Richardson <cathyanne08@gmail.com>
2021-05-22 16:28:14 -0600

    completed all but reset of tic-tac-toe

--- Part1/index.js
+++ Part1/index.js
@@ -2,17 +2,59 @@ console.log("js is connected");
 let gameProgress = [];
 
 function getWinner() {
-    
+
+    if (gameProgress[0] !== undefined
+        && gameProgress[0] === gameProgress[1]
+        && gameProgress[0] === gameProgress[2]) {
+        return gameProgress[0];
+    }
+    if (gameProgress[3] !== undefined
+        && gameProgress[3] === gameProgress[4]
+        && gameProgress[3] === gameProgress[5]) {
+        return gameProgress[3];
+    }
+    if (gameProgress[6] !== undefined
+        && gameProgress[6] === gameProgress[7]
+        && gameProgress[6] === gameProgress[8]) {
+        return gameProgress[6];
+    }
+    if (gameProgress[0] !== undefined
+        && gameProgress[0] === gameProgress[3]
+        && gameProgress[0] === gameProgress[6]) {
+        return gameProgress[0];
+    }
+    if (gameProgress[1] !== undefined
+        && gameProgress[1] === gameProgress[4]
+        && gameProgress[1] === gameProgress[7]) {
+        return gameProgress[1];
+    }
+    if (gameProgress[2] !== undefined
+        && gameProgress[2] === gameProgress[5]
+        && gameProgress[2] === gameProgress[8]) {
+        return gameProgress[2];
+    }   
+    if (gameProgress[0] !== undefined
+        && gameProgress[0] === gameProgress[4]
+        && gameProgress[0] === gameProgress[8]) {
+        return gameProgress[0];
+    } 
+    if (gameProgress[2] !== undefined
+        && gameProgress[2] === gameProgress[4]
+        && gameProgress[2] === gameProgress[6]) {
+              return gameProgress[2];
+    } 
+    return;
 }
 
 
 function isCatsGame() {
     let catsGame = true;
-    gameProgress.forEach(e => {
-        if (e === undefined) {
+    console.log("game: ", gameProgress);
+    for (let i = 0; i < 9; i++) {
+        if (gameProgress[i] === undefined) {
             catsGame = false;
         }
-    });
+    }
     return catsGame;
 }
 
@@ -39,6 +81,8 @@ function play(id) {
     } else {
         playerSpan.innerText = "X";
     }
-    console.log(gameProgress);
+
+
 }
+
 
