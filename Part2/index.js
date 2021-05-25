d2b25f4cc95000a8e7989f6f038e61924219721f
Cathy Richardson <cathyanne08@gmail.com>
2021-05-24 19:16:56 -0600

    completed reset of poker

--- Part2/index.js
+++ Part2/index.js
@@ -5,8 +5,13 @@ let color = document.getElementById(cardStyle);
 
 console.log("cardID: ", card, "cardStyle: ", color);
 
-function setCard(params) {
+function setCard() {
     let card = document.getElementById(cardId.value);
     card.style.color = cardStyle.value;
     console.log(card);
+}
+
+function reset() {
+    // document.querySelectorAll("section").forEach( e => {console.log("e: ", e)});
+    document.querySelectorAll("section").forEach( e => {e.style.color = "grey"});
 }
